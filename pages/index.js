424ec2c7577043f68158106b451a5d6d46e1e2c9
steven43533 <steven43533@gmail.com>
2022-01-04 01:36:34 -0500

    Added navbar with tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello!</p>
-      </section>
-    </Layout>
+    <div>
+      <NavBar />
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        
+        <section className={utilStyles.headingMd}>
+          <p>Hello!</p>
+        </section>
+      </Layout>
+    </div>
+
   )
 }
